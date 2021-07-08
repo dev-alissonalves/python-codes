01086ce8b43a5601c02257ca7270193fb630bb03
Álisson Alves <86388002+dev-alissonalves@users.noreply.github.com>
2021-07-07 22:03:39 -0300

    Update ex090.py

--- scripts/6-estrutura-dados-python/dicionarios-python/ex090.py
+++ scripts/6-estrutura-dados-python/dicionarios-python/ex090.py
@@ -1,4 +1,5 @@
-#Exercício Python 090: Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário. No final, mostre o conteúdo da estrutura na tela
+#Exercício Python 090: Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário. 
+#No final, mostre o conteúdo da estrutura na tela
 
 aluno = {}
 aluno["nome"] = str(input("Informe o nome do aluno: "))
