9faec53bc4ecc25f3bed8bd6c585243917b791c3
Álisson Alves <alissoncc.oliveira@gmail.com>
2021-07-09 08:45:30 -0300

    update code

--- scripts/7-funcoes-python/ex100.py
+++ scripts/7-funcoes-python/ex100.py
@@ -1,4 +1,5 @@
 #Exercício Python 100: Faça um programa que tenha uma lista chamada números e duas funções chamadas sorteia() e somaPar(). A primeira função vai sortear 5 números e vai colocá-los dentro da lista e a segunda função vai mostrar a soma entre todos os valores pares sorteados pela função anterior.
+
 from random import randint
 def somaPar(lista):
     soma = 0
@@ -6,7 +7,6 @@ def somaPar(lista):
         if c %2 == 0:
             soma += c
     print(f"A soma total dos números pares foi: {soma}")
-
 
 def sorteia():
     for c in range(1, 6):
